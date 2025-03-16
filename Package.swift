fd3c92ed6e121ca7f830092ba90595b8a50f63cb
Mark Alldritt <alldritt@latenightsw.com>
2025-03-16 10:27:24 -0700

    Increase iOS version requirement to iOS 17

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "LNSWeekView",
     platforms: [
-        .iOS(.v16), .watchOS(.v9)
+        .iOS(.v17), .watchOS(.v9)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
